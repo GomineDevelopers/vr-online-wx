00738b221007fa511822305bb12f4c50a2c7f2b1
shaossss <969348836@qq.com>
2018-09-03 14:57:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import {
-  router
-} from '@/router';
+import {router} from '@/router';
 import Vant from 'vant';
 import 'vant/lib/vant-css/index.css';
 /*引入公共样式*/
 import '../static/css/common.css'
+
 Vue.use(Vant);
 Vue.config.productionTip = false
 
