7c4332c53fb96dbe2e5d0f2fb9e360084711325f
Arron Zhu <1274294199@qq.com>
2018-09-03 14:46:30 +0800

    路由书写格式改变

--- src/main.js
+++ src/main.js
@@ -2,20 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import {
+  router
+} from '@/router';
 import Vant from 'vant';
 import 'vant/lib/vant-css/index.css';
 /*引入公共样式*/
 import '../static/css/common.css'
-
 Vue.use(Vant);
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import registerOne from '@/pages/registerone'
-import registerTwo from '@/pages/registertwo'
+Vue.use(Router);
+const routes = [{
+    name: 'HelloWorld',
+    component: () =>import('@/components/HelloWorld'),
+    path: '/',
+    meta: {
+      title: '测试helloWorld页面'
+    }
+  },
+  {
+    name: 'registerOne',
+    component: () =>import('@/pages/registerone'),
+    path: '/registerone',
+    meta: {
+      title: '注册页面一'
+    }
+  },
+  {
+    name: 'registerTwo',
+    component: () =>import('@/pages/registertwo'),
+    path: '/registertwo',
+    meta: {
+      title: '注册页面二'
+    }
+  }
+];
 
-Vue.use(Router)
+// add route path
+routes.forEach(route => {
+  route.path = route.path || '/' + (route.name || '');
+});
+const router = new Router({
+  routes
+});
+router.beforeEach((to, from, next) => {
+  const title = to.meta && to.meta.title;
+  if (title) {
+    document.title = title;
+  }
+  next();
+});
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/registerone',
-      name: 'registerOne',
-      component: registerOne
-    },
-    {
-      path: '/registertwo',
-      name: 'registerTwo',
-      component: registerTwo
-    },
-  ]
-})
+export {
+  router
+};
