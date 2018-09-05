9ff979d2d6bee92b66ddc0410b62c6b0442f144c
shaossss <969348836@qq.com>
2018-09-05 17:45:08 +0800

    update

--- src/pages/problemconsult.vue
+++ src/pages/problemconsult.vue
@@ -4,14 +4,14 @@
       <div class="title">
           <van-row type="flex" justify="center">
             <van-col span="24" v-text="consultUserName"></van-col>
-            </van-row>
+          </van-row>
       </div>
   </div>
   <div class="problemConsult-content">
       <van-row type="flex" justify="center" class="row-wrapper">
         <van-col span="20">
             <van-row type="flex">
-                <van-col span="12"> <div class="sub-title">问题咨询</div> </van-col>    
+                <van-col span="12"> <div class="sub-title">问题咨询</div> </van-col>
                 <van-col span="12"><van-button class="consult-record" type="primary" plain>咨询记录</van-button></van-col>
             </van-row>
         </van-col>
@@ -19,36 +19,32 @@
         <van-row type="flex" justify="center" class="row-wrapper">
         <van-col span="20">
             <van-row type="flex">
-                <van-col span="24"> <div class="sub-comment">请填写您的问题并提交:</div> </van-col>    
-               
+                <van-col span="24"> <div class="sub-comment">请填写您的问题并提交:</div> </van-col>
             </van-row>
-        </van-col> 
-      
+        </van-col>
+
       </van-row>
       <van-row type="flex" justify="center" class="row-wrapper">
         <van-col span="20">
             <van-row type="flex">
-                <van-col span="24"> 
-                <textarea class="consult-inputBox" v-model="problemContent" maxlength="400"  placeholder="请输入您的问题~">
-                              
-                </textarea> 
-                 <div class="count">
-                     <span v-text="problemContent.length"></span>
-                     <span>/</span>
-                     <span>400</span>
-                 </div>
-                </van-col>    
-               
+                <van-col span="24">
+                  <textarea class="consult-inputBox" v-model="problemContent" maxlength="400"  placeholder="请输入您的问题~"></textarea>
+                   <div class="count">
+                       <span v-text="problemContent.length"></span>
+                       <span>/</span>
+                       <span>400</span>
+                   </div>
+                </van-col>
             </van-row>
-        </van-col> 
+        </van-col>
       </van-row>
-        <van-row type="flex" justify="center" class="row-wrapper bottom-button">
-                 <van-col span="20">
-                    <div class="bottomDiv">
-                        <van-button type="primary" size="large">提交</van-button>
-                    </div>  
-                </van-col> 
-        </van-row> 
+      <van-row type="flex" justify="center" class="row-wrapper bottom-button">
+         <van-col span="20">
+            <div class="bottomDiv">
+              <van-button type="primary" size="large">提交</van-button>
+            </div>
+        </van-col>
+      </van-row>
   </div>
  </div>
 </template>
