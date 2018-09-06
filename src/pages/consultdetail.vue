8147f9dffee40c8e76685922b08924eac288f483
Arron Zhu <1274294199@qq.com>
2018-09-06 10:46:26 +0800

    更新阴影效果
    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);

--- src/pages/consultdetail.vue
+++ src/pages/consultdetail.vue
@@ -27,99 +27,101 @@
 </template>
 
 <script>
-    export default {
-        name: "consultdetail"
-    }
+export default {
+  name: "consultdetail"
+};
 </script>
 
 <style scoped>
-  .bgCol{
-    background-color: #eff1f5;
-    height: 100vh;
-    overflow-x: hidden;
-    overflow-y: hidden;
-  }
+.bgCol {
+  background-color: #eff1f5;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
 
-  .problem{
-    text-align: left;
-    background: #ffffff;
-    -webkit-box-shadow: 0px 0px 20px 5px #e9e9e9;
-    box-shadow: 0px 0px 20px 5px #e9e9e9;
-    position: relative;
-    border-radius: 5px;
-    margin: 2vh;
-    min-height: 11vh;
-  }
+.problem {
+  text-align: left;
+  background: #ffffff;
+  -webkit-box-shadow: 0px 0px 20px 5px #e9e9e9;
+  box-shadow: 0px 0px 20px 5px #e9e9e9;
+  position: relative;
+  border-radius: 5px;
+  margin: 2vh;
+  min-height: 11vh;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
 
-  .problemContent{
-    padding: 2vh 5vh 2vh 2vh;
-  }
+.problemContent {
+  padding: 2vh 5vh 2vh 2vh;
+}
 
-  .problem .c1 {
-    width: 0;
-    height: 0;
-    border-left: 7.5vh solid transparent;
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
+.problem .c1 {
+  width: 0;
+  height: 0;
+  border-left: 7.5vh solid transparent;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 
-  .problem .c2 {
-    width: 0;
-    height: 0;
-    border-top: 3vh solid white;
-    border-left: 3vh solid transparent;
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
+.problem .c2 {
+  width: 0;
+  height: 0;
+  border-top: 3vh solid white;
+  border-left: 3vh solid transparent;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 
-  .problem .c3 {
-    top: 1vh;
-    right: -1.3vh;
-    color: white;
-    position: absolute;
-    width: 8vh;
-    height: 3.6vh;
-    line-height: 3.6vh;
-    -webkit-transform: rotate(45deg);
-    transform: rotate(45deg) scale(0.9);
-    z-index: 111;
-    font-size: 2vw;
-    text-align: center;
-    letter-spacing: 0.5px;
-  }
+.problem .c3 {
+  top: 1vh;
+  right: -1.3vh;
+  color: white;
+  position: absolute;
+  width: 8vh;
+  height: 3.6vh;
+  line-height: 3.6vh;
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg) scale(0.9);
+  z-index: 111;
+  font-size: 2vw;
+  text-align: center;
+  letter-spacing: 0.5px;
+}
 
-  .status-success .c1 {
-    border-top: 7.5vh solid #66c661;
-  }
+.status-success .c1 {
+  border-top: 7.5vh solid #66c661;
+}
 
-  .status-wait .c1 {
-    border-top: 7.5vh solid #d26e7a;
-  }
+.status-wait .c1 {
+  border-top: 7.5vh solid #d26e7a;
+}
 
-  .time{
-    margin: 2vh 1vh 0 0;
-    color: #808080;
-    font-size: 12px;
-  }
+.time {
+  margin: 2vh 1vh 0 0;
+  color: #808080;
+  font-size: 12px;
+}
 
-  .logoDiv{
-    margin: 2vh 2vh 1vh 2vh;
-  }
+.logoDiv {
+  margin: 2vh 2vh 1vh 2vh;
+}
 
-  .logo{
-    width: 32px;
-  }
+.logo {
+  width: 32px;
+}
 
-  .answerContent{
-    background-color: #fff;
-    margin: 0 2vh;
-    padding: 2vh;
-    border-radius: 5px;
-  }
+.answerContent {
+  background-color: #fff;
+  margin: 0 2vh;
+  padding: 2vh;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
 
-  .doctorName{
-    text-align: right;
-  }
+.doctorName {
+  text-align: right;
+}
 </style>
